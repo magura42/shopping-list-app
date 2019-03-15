65d9b66dc59bba4a860b1ad3df8392948cdb2e0e
Manfred Harrer <manfred.harrer@jambit.com>
2019-03-15 15:30:20 +0100

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import HelloWorld from './components/HelloWorld';
-import HelloWorldFunctional, {A} from "./components/HelloWorldFunctional";
-import User from "./components/User"
-import {IUserProperties} from "./components/IUserProperties";
-import UserList from "./components/UserList";
 
-let defaultUsers: Array<IUserProperties> = [
-  {lastname: "harrer", firstname: "manfred"}, {lastname: "müller", firstname: "fritz"}
-];
 
 ReactDOM.render(
     <div>
-      UserList:
-      <UserList users={defaultUsers} />
-      User:
-      <User lastname="harrer" firstname="manfred"></User>
-
-      <HelloWorld/>
-      Antwort: {A}
-      <HelloWorldFunctional/>
     </div>
   , document.getElementById("start")
 );
